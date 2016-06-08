04d3674250d93c54b818da0c9943f9c97adf84fe
Stephen_Chang <3130100653@zju.edu.cn>
2016-06-08 20:17:18 +0800

    update test10.pas & add readln

--- TestCases/test10.pas
+++ TestCases/test10.pas
@@ -5,38 +5,91 @@ const
     PI=3.14 ;
     fir = 1;
     sec = 10;
+    LARGE = 10000202020202;
 
 type
-    arr = array[string] of integer;
+    arr = array[1..10] of integer;
+    car = record
+            number : string;
+            legal : boolean;
+            level : integer;
+        end;
 
 var
+    a, b, c, n:integer;
     arr_var: arr;
-hh : integer;
+    hhh : integer;
+    book: record
+        title : integer;
+        author : integer;
+        content : string;
+        pal : boolean;
+    end;
+    cc : car;
+    test_str1 : string;
+
+procedure findMin(x, y:integer; var m:integer);
+begin
+
+    if x<y then
+        m:=x
+    else
+        m:=y;
+end;
+
+procedure proc1;
+begin
+end;
+
+function func1:integer;
+begin
+end;
 
 begin
-//    readln(n);
-//    case n of
-//    1: writeln(0);
-//    2: writeln(1);
-//    end;
-//    arr[1] =10;
-//    arr_var[11]:=10;
-    writeln(sizeof(arr_var));
-    writeln(sizeof(integer));
-//    1:
-//    a:=1;
-//    b:=2;
-//    goto 1;
+    read(a);
+    readln(n);
+    case n of
+    1: writeln(0);
+    2: writeln(1);
+    3: begin
+        a := 1;
+        b := 2;
+        end;
+    end;
 
-//    while true do
-//    begin
-//        a:=1;
-//        b:=1;
-//        c:=2;
-//    end;
+    arr_var[10]:=10; 
+
+    while true do
+    begin
+        a:=1;
+        b:=1;
+        c:=2;
+    end;
+
+    proc1;
+    func1;
+
+    book.title := 1;
+    book.content := '123';
+
+    a:=book.title;
+    test_str1 := book.content;
+
+    begin
+        a:=1;
+        b:=1;
+        c:=2;
+    end;
+
+        if (a > b) then
+            c := a;
+ 	hhh := (a+b) * a;
+
+	findMin(a,b,c);
 
 
-//    hhh := (a+b) * a;
-
-
+        for a:= 1*2 to 100 do begin
+            c:= c + 1;	
+	end;
+   
 end.
