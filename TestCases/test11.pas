a5dacb2ecef6155f1cf32aefd41c6da89f6aeb70
Stephen_Chang <3130100653@zju.edu.cn>
2016-06-08 12:58:25 +0800

    update readme.md

--- TestCases/test11.pas
+++ TestCases/test11.pas
@@ -1,12 +1,23 @@
 program test4;
 
 var 
-x,y:longint;
+x, y: longint;
 a, c: longint;
 b, d: dword;
 arr : array[0..10] of integer;
 e, f: integer;
+str : string;
 
+function max(num2: string): integer;
+
+begin
+    max := 1;
+end;
+
+function test:integer;
+begin
+    test:=1;
+end;
 
 begin
 a:= 2147483645;
@@ -15,7 +26,7 @@ c:=-10;
 d:=5;
 
 x := a+b;
-writeln(a+b);
+writeln(x);
 
 y:=c+d;
 writeln(y);
@@ -27,5 +38,12 @@ arr[0] := 10;
     f := 5;
     e := e DIV f;
     writeln(e);
+
+    str := '123';
+    max(str);
+    writeln(str);
+
+    a := test;
+    writeln(a);
 
 end.
